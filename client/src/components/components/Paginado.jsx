5add039df60799fe47caa1846c6759842e1920ce
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-04-26 03:25:13 -0300

    estilos paginado

--- client/src/components/components/Paginado.jsx
+++ client/src/components/components/Paginado.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './Paginado.css'
 
 
 export const Paginado = ({pokemonsPerPage, pokemons, paginado}) =>{
@@ -8,12 +9,26 @@ export const Paginado = ({pokemonsPerPage, pokemons, paginado}) =>{
         pageNumbers.push(i);
     }
 
+    const elementosLista = document.querySelectorAll('.lista-elemento');
+
+function cambiarEstilos() {
+  elementosLista.forEach(elemento => {
+    elemento.classList.remove('seleccionado');
+  });
+
+  this.classList.add('seleccionado');
+}
+
+elementosLista.forEach(elemento => {
+  elemento.addEventListener('click', cambiarEstilos);
+});
+
     return(
         <nav>
-            <ul>
+            <ul className="div-lista">
                 {pageNumbers && pageNumbers.map(number=>(
                     // <li key = {number}>
-                    <a onClick={()=>paginado(number)}>  {number}  </a>
+                    <span id={`elemento${number}`} class="lista-elemento" onClick={()=>paginado(number)}>  {number}  </span>
                     // </li>
                 ))}
             </ul>
